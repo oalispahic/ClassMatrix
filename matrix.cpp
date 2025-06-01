a2d332a23d0f561de14a35a903f36a05ef95c481
Omar Alispahic <omaralispahic@Omars-MacBook-Pro.local>
2025-06-01 13:08:06 +0200

    Testing features

--- matrix.cpp
+++ matrix.cpp
@@ -3,12 +3,11 @@
 #include <iostream>
 #include <vector>
 int main(){
-    matrix matrica = matrix(5,6,1000);
+    matrix matrica = matrix(5,6,0);
     matrica.print();
     std::cout<<std::endl;
-    auto number = matrica.at(1);
-    std::vector<std::vector<int>> matrixV (10, std::vector<int>(10,5));
-    auto mat = matrixV.at(0).at(0);
+    auto number = matrica.nth(31);
+    std::cout<<(number);
 
 
     return 0;
