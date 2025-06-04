d7428214823954a084a0182370d6f1679720aef4
Omar Alispahic <omaralispahic@Omars-MacBook-Pro.local>
2025-06-04 21:56:36 +0200

    Just added code for initializing the matrix with new constructor

--- matrix.cpp
+++ matrix.cpp
@@ -3,11 +3,15 @@
 #include <iostream>
 #include <vector>
 int main(){
-    matrix matrica = matrix(5,6,0);
+   /* matrix matrica = matrix(5,6,1);
     matrica.print();
     std::cout<<std::endl;
     auto number = matrica.nth(31);
-    std::cout<<(number);
+    std::cout<<(number);*/
+
+   matrix test = {{1,2,3},{1,2,3},{1,2,3}};
+   test.print();
+
 
 
     return 0;
